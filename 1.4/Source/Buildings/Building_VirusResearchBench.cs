ff84d6e221634684f76523c491f74f9c020471a7
Aaron Karlsen <aaronkarlsen@KarlsenFamily.onmicrosoft.com>
2023-07-02 10:30:05 +1000

    Early concept for the virus research work

---
+++ 1.4/Source/Buildings/Building_VirusResearchBench.cs
@@ -0,0 +1,64 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+
+using Verse;
+
+namespace Pandemics
+{
+    public class Building_VirusResearchBench : ThingWithComps
+    {
+        public override string GetInspectString()
+        {
+            // Add custom inspect string for the virus research bench
+            return base.GetInspectString() + "\n" + "Researching unknown viruses.";
+        }
+
+        public override IEnumerable<Gizmo> GetGizmos()
+        {
+            // Add custom Gizmos for the virus research bench
+            foreach (var gizmo in base.GetGizmos())
+            {
+                yield return gizmo;
+            }
+        }
+
+        public override void Tick()
+        {
+            base.Tick();
+
+            if (!Spawned)
+            {
+                return;
+            }
+
+            HediffDef virusHediffDef = DefDatabase<HediffDef>.GetNamed("Pandemic_Virus_UnknownVirus");
+
+            // Check if any pawns are currently assigned to the virus research work type
+            var assignedPawns = Find.CurrentMap.mapPawns.FreeColonistsSpawned.Where(p => p.workSettings?.GetPriority(WorkTypeDefOf.Research) > 0);
+            foreach (var pawn in assignedPawns)
+            {
+                // Check if the pawn has the "Pandemic_Virus_UnknownVirus" hediff
+                var unknownVirusHediff = pawn.health.hediffSet.GetFirstHediffOfDef(virusHediffDef);
+                if (unknownVirusHediff != null)
+                {
+                    // Generate a new unique HediffDef for the virus
+                    var uniqueSuffix = GenText.StableStringHash(pawn.GetUniqueLoadID());
+                    var newVirusHediffDef = new HediffDef
+                    {
+                        defName = "Pandemic_Virus_" + uniqueSuffix,
+                        label = "COV-19",
+                        // Add any other properties you want to set for the new HediffDef
+                    };
+
+                    // Replace the existing hediff with the newly generated HediffDef
+                   pawn.health.RemoveHediff(unknownVirusHediff);
+
+                // Add the new hediff to the pawn
+                var newVirusHediff = HediffMaker.MakeHediff(newVirusHediffDef, pawn);
+                pawn.health.AddHediff(newVirusHediff);
+                }
+            }
+        }
+    }
+}
---
+++ 1.4/Source/Properties/HediffDefOf.cs
@@ -0,0 +1,11 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Pandemics
+{
+    [DefOf]
+    public static class HediffDefOf
+    {
+        public static HediffDef Pandemic_Virus_UnknownVirus;
+    }
+}
