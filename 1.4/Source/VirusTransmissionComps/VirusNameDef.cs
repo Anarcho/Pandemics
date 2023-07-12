a1d381f2f68ea0dec3fad98f2e392e3461d73dbe
InstantKrill <aaronkarlsen@gmail.com>
2023-07-13 08:17:57 +1000

    fixing up VirusNameDef

--- 1.4/Source/VirusTransmissionComps/VirusNameDef.cs
+++ 1.4/Source/VirusTransmissionComps/VirusNameDef.cs
@@ -3,9 +3,20 @@ using System.Collections.Generic;
 
 namespace Pandemics
 {
-    public class VirusNameDef : ThingDef
+    public class VirusNameDef : Def
     {
         public List<string> prefixes;
         public List<string> suffixes;
+
+        public override void ResolveReferences()
+        {
+            base.ResolveReferences();
+
+            if (prefixes == null)
+                prefixes = new List<string>();
+
+            if (suffixes == null)
+                suffixes = new List<string>();
+        }
     }
 }
